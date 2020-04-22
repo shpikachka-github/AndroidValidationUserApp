cfa8cf1b2aed0411539fa149bbbe3895b15bad37
nano <shpikachka.p@gmail.com>
2020-04-22 10:55:06 +0400

    Update MainActivity.java

--- app/src/main/java/org/nano/androidvalidationuserapp/MainActivity.java
+++ app/src/main/java/org/nano/androidvalidationuserapp/MainActivity.java
@@ -60,157 +60,157 @@ public class MainActivity extends AppCompatActivity {
     private TextWatcher watcherName = new TextWatcher() {
         @Override
         public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-        }
-
-        @Override
-        public void onTextChanged(CharSequence s, int start, int before, int count) {
             textInputLayoutName.setError(null);
         }
 
         @Override
-        public void afterTextChanged(Editable s) {
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            validateName();
+        }
 
+        @Override
+        public void afterTextChanged(Editable s) {
+            onConfirm();
         }
     };
 
     private TextWatcher watcherEmail = new TextWatcher() {
         @Override
         public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-        }
-
-        @Override
-        public void onTextChanged(CharSequence s, int start, int before, int count) {
             textInputLayoutEmail.setError(null);
         }
 
         @Override
-        public void afterTextChanged(Editable s) {
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            validateEmail();
+        }
 
+        @Override
+        public void afterTextChanged(Editable s) {
+            onConfirm();
         }
     };
 
     private TextWatcher watcherPassword = new TextWatcher() {
         @Override
         public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-        }
-
-        @Override
-        public void onTextChanged(CharSequence s, int start, int before, int count) {
             textInputLayoutPassword.setError(null);
         }
 
         @Override
-        public void afterTextChanged(Editable s) {
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            validatePassword();
+        }
 
+        @Override
+        public void afterTextChanged(Editable s) {
+            onConfirm();
         }
     };
 
     private TextWatcher watcherPasswordConfirm = new TextWatcher() {
         @Override
         public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-        }
-
-        @Override
-        public void onTextChanged(CharSequence s, int start, int before, int count) {
             textInputLayoutPasswordConfirm.setError(null);
         }
 
         @Override
-        public void afterTextChanged(Editable s) {
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            validatePasswordConfirm();
+        }
 
+        @Override
+        public void afterTextChanged(Editable s) {
+            onConfirm();
         }
     };
 
     private View.OnFocusChangeListener onConfirmName = (v, hasFocus) -> {
-        if (!hasFocus)
-            validateName();
-        else
+        if (!hasFocus) {
+            String inputName = Objects.requireNonNull(textInputLayoutName.getEditText()).getText().toString().trim();
+            if (inputName.isEmpty()) {
+                textInputLayoutName.setError("Field can't be empty");
+            } else if (inputName.length() > 8) {
+                textInputLayoutName.setError("Name too long");
+            } else if (inputName.length() <= 1) {
+                textInputLayoutName.setError("Name must be longer than 1 charter");
+                isConfirmName = false;
+            } else {
+                textInputLayoutName.setError(null);
+            }
+        } else
             onConfirm();
     };
 
     private View.OnFocusChangeListener onConfirmEmail = (v, hasFocus) -> {
-        if (!hasFocus)
-            validateEmail();
-        else
+        if (!hasFocus) {
+            String inputEmail = Objects.requireNonNull(textInputLayoutEmail.getEditText()).getText().toString().trim();
+            if (inputEmail.isEmpty()) {
+                textInputLayoutEmail.setError("Field can't be empty");
+            } else {
+                textInputLayoutEmail.setError(null);
+            }
+        } else
             onConfirm();
     };
 
     private View.OnFocusChangeListener onConfirmPassword = (v, hasFocus) -> {
-        if (!hasFocus)
-            validatePassword();
-        else
+        if (!hasFocus) {
+            String inputPassword = Objects.requireNonNull(textInputLayoutPassword.getEditText()).getText().toString().trim();
+            if (inputPassword.isEmpty()) {
+                textInputLayoutPassword.setError("Field can't be empty");
+            } else {
+                textInputLayoutPassword.setError(null);
+            }
+        } else
             onConfirm();
     };
 
     private View.OnFocusChangeListener onConfirmPasswordTwo = (v, hasFocus) -> {
-        if (!hasFocus)
-            validatePasswordConfirm();
-        else
+        if (!hasFocus) {
+            String inputPasswordConfirm = Objects.requireNonNull(textInputLayoutPasswordConfirm.getEditText()).getText().toString().trim();
+            if (inputPasswordConfirm.isEmpty()) {
+                textInputLayoutPasswordConfirm.setError("Field can't be empty");
+            } else if (!inputPasswordConfirm.equals(Objects.requireNonNull(textInputLayoutPassword.getEditText()).getText().toString().trim())) {
+                textInputLayoutPasswordConfirm.setError("Passwords is different");
+            } else {
+                textInputLayoutPasswordConfirm.setError(null);
+            }
+        } else
             onConfirm();
     };
+
+    private void validateName() {
+        String inputName = Objects.requireNonNull(textInputLayoutName.getEditText()).getText().toString().trim();
+        if (inputName.isEmpty()) {
+            isConfirmName = false;
+        } else if (inputName.length() > 8) {
+            isConfirmName = false;
+        } else isConfirmName = inputName.length() > 1;
+    }
+
+    private void validateEmail() {
+        String inputEmail = Objects.requireNonNull(textInputLayoutEmail.getEditText()).getText().toString().trim();
+        isConfirmEmail = !inputEmail.isEmpty();
+    }
+
+    private void validatePassword() {
+        String inputPassword = Objects.requireNonNull(textInputLayoutPassword.getEditText()).getText().toString().trim();
+        isConfirmPassword = !inputPassword.isEmpty();
+    }
+
+    private void validatePasswordConfirm() {
+        String inputPasswordConfirm = Objects.requireNonNull(textInputLayoutPasswordConfirm.getEditText()).getText().toString().trim();
+        if (inputPasswordConfirm.isEmpty()) {
+            isConfirmPasswordTwo = false;
+        } else isConfirmPasswordTwo = inputPasswordConfirm.equals(Objects.requireNonNull(textInputLayoutPassword.getEditText()).getText().toString().trim());
+    }
 
     private void onConfirm() {
         if (isConfirmName & isConfirmEmail & isConfirmPassword & isConfirmPasswordTwo)
             buttonSave.setEnabled(true);
         else
             buttonSave.setEnabled(false);
-    }
-
-    private void validateName() {
-        String inputName = Objects.requireNonNull(textInputLayoutName.getEditText()).getText().toString().trim();
-        if (inputName.isEmpty()) {
-            textInputLayoutName.setError("Field can't be empty");
-            isConfirmName = false;
-        } else if (inputName.length() > 8) {
-            textInputLayoutName.setError("Name too long");
-            isConfirmName = false;
-        } else if (inputName.length() <= 2) {
-            textInputLayoutName.setError("Name too short");
-            isConfirmName = false;
-        } else {
-            textInputLayoutName.setError(null);
-            isConfirmName = true;
-        }
-    }
-
-    private void validateEmail() {
-        String inputEmail = Objects.requireNonNull(textInputLayoutEmail.getEditText()).getText().toString().trim();
-        if (inputEmail.isEmpty()) {
-            textInputLayoutEmail.setError("Field can't be empty");
-            isConfirmEmail = false;
-        } else {
-            textInputLayoutEmail.setError(null);
-            isConfirmEmail = true;
-        }
-    }
-
-    private void validatePassword() {
-        String inputPassword = Objects.requireNonNull(textInputLayoutPassword.getEditText()).getText().toString().trim();
-        if (inputPassword.isEmpty()) {
-            textInputLayoutPassword.setError("Field can't be empty");
-            isConfirmPassword = false;
-        } else {
-            textInputLayoutPassword.setError(null);
-            isConfirmPassword = true;
-        }
-    }
-
-    private void validatePasswordConfirm() {
-        String inputPasswordConfirm = Objects.requireNonNull(textInputLayoutPasswordConfirm.getEditText()).getText().toString().trim();
-        if (inputPasswordConfirm.isEmpty()) {
-            textInputLayoutPasswordConfirm.setError("Field can't be empty");
-            isConfirmPasswordTwo = false;
-        } else if (!inputPasswordConfirm.equals(Objects.requireNonNull(textInputLayoutPassword.getEditText()).getText().toString().trim())) {
-            textInputLayoutPasswordConfirm.setError("Passwords is different");
-            isConfirmPasswordTwo = false;
-        } else {
-            textInputLayoutPasswordConfirm.setError(null);
-            isConfirmPasswordTwo = true;
-        }
     }
 
     public void onClickSave(View view) {
