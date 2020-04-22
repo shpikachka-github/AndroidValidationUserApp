e440f64c9468e7eed831b478582841c81f1f1839
nano <shpikachka.p@gmail.com>
2020-04-22 12:54:25 +0400

    Update SaveActivity.java

--- app/src/main/java/org/nano/androidvalidationuserapp/SaveActivity.java
+++ app/src/main/java/org/nano/androidvalidationuserapp/SaveActivity.java
@@ -6,28 +6,35 @@ import android.os.Bundle;
 import android.widget.TextView;
 import androidx.annotation.Nullable;
 
-import java.util.Objects;
-
 public class SaveActivity extends Activity {
 
     private String name;
     private String email;
     private String password;
 
+    private TextView textViewName;
+    private TextView textViewEmail;
+    private TextView textViewPassword;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_save);
 
-        TextView textViewName = findViewById(R.id.textViewName);
-        TextView textViewEmail = findViewById(R.id.textViewEmail);
-        TextView textViewPassword = findViewById(R.id.textViewPassword);
+        textViewName = findViewById(R.id.textViewName);
+        textViewEmail = findViewById(R.id.textViewEmail);
+        textViewPassword = findViewById(R.id.textViewPassword);
 
         Intent intent = getIntent();
 
         name = intent.getStringExtra(name);
         email = intent.getStringExtra(email);
         password = intent.getStringExtra(password);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
 
         textViewName.setText(name);
         textViewEmail.setText(email);
